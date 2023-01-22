410936ede467b87f8c843c803fb06cf1c5988300
rhemming <rhemming@redhat.com>
2023-01-21 20:35:32 -0500

    MSINTEROP-226 Initial commit

--- utilities/infra.py
+++ utilities/infra.py
@@ -3,10 +3,12 @@ import logging
 from ocm_python_wrapper.ocm_client import OCMPythonClient
 from ocp_utilities.exceptions import (
     NodeNotReadyError,
+    NodesNotHealthyConditionError,
     NodeUnschedulableError,
     PodsFailedOrPendingError,
 )
 from ocp_utilities.infra import (
+    assert_nodes_in_healthy_condition,
     assert_nodes_ready,
     assert_nodes_schedulable,
     assert_pods_failed_or_pending,
@@ -27,9 +29,11 @@ def cluster_sanity(
     """
     Args:
         nodes (list): list of Node resources
+        pods (list): list of Pod resources
 
     Raises:
-        NodeNotReadyError or NodeUnschedulableError if node check failed
+        NodeNotReadyError, NodeUnschedulableError, PodsFailedOrPendingError, or
+        NodesNotHealthyConditionError if node check failed
     """
 
     exceptions_filename = "cluster_sanity_failure.txt"
@@ -39,9 +43,16 @@ def cluster_sanity(
         LOGGER.info("Check nodes sanity.")
         assert_nodes_ready(nodes=nodes)
         assert_nodes_schedulable(nodes=nodes)
+        assert_nodes_in_healthy_condition(nodes=nodes)
         assert_pods_failed_or_pending(pods=pods)
 
-    except (NodeNotReadyError, NodeUnschedulableError, PodsFailedOrPendingError) as ex:
+    except (
+        NodeNotReadyError,
+        NodeUnschedulableError,
+        PodsFailedOrPendingError,
+        NodesNotHealthyConditionError,
+    ) as ex:
+
         exit_pytest_execution(
             filename=exceptions_filename,
             message=ex.args[0],
