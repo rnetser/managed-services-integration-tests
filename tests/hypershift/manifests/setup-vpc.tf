f72e42b11f194b06eb7b5c6b7bb3b09204f4a65a
null <108792105+redhat-qe-bot@users.noreply.github.com>
2023-05-30 15:57:05 +0000

    Update Terraform terraform-aws-modules/vpc/aws to v5

--- tests/hypershift/manifests/setup-vpc.tf
+++ tests/hypershift/manifests/setup-vpc.tf
@@ -19,7 +19,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "< 5.0.0"
+  version = "< 6.0.0"
 
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
